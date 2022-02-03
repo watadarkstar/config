7b3c0798a1087d57a9d1de80b77adc6338337b7b
Adrian Carolli <adrian.caarolli@gmail.com>
2022-02-03 12:13:30 -0500

    feat: add fish config

---
+++ fish/config.fish
@@ -0,0 +1,20 @@
+echo "~/.config/fish/config.fish imported"
+bass source ~/.bash_profile
+
+# Git Aliases
+# migrating from https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/git/git.plugin.zsh
+echo "Git Aliases imported: https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/git/git.plugin.zsh"
+alias g='git'
+alias gs='git status'
+alias gst='git status'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias gl='git pull'
+alias gup='git pull --rebase'
+alias gp='git push'
+alias gd='git diff'
+alias ga='git add'
+alias gcm='git checkout master'
+
+echo "Netlify Aliases imported"
+alias nwatch='netlify watch && say "done mr. adrian"'
